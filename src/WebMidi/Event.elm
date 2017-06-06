3dfd1e9777b13aa8eebd3ed25b18940a08bc479a
Billy Keyes <bluekeyes@gmail.com>
2017-06-05 23:08:27 -0700

    Add WebMidi.Event to hold event and message types

---
+++ src/WebMidi/Event.elm
@@ -0,0 +1,37 @@
+module WebMidi.Event exposing
+    ( Event
+    , Channel, Message(..)
+    )
+
+{-| Event type for the Web MIDI API.
+
+@docs Event, Channel, Message
+
+-}
+
+import Time exposing (Time)
+
+
+{-| A MIDI event, a message at a specific time.
+-}
+type alias Event =
+    { time : Time
+    , message : Message
+    }
+
+
+{-| A MIDI channel (1 - 16).
+-}
+type alias Channel = Int
+
+
+{-| A MIDI message.
+-}
+type Message
+    = NoteOff Channel Int Int
+    | NoteOn Channel Int Int
+    | KeyPressure Channel Int Int
+    | ControlChange Channel Int Int
+    | ProgramChange Channel Int
+    | ChannelPressure Channel Int
+    | PitchBend Channel Int

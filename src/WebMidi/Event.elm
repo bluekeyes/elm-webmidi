384003ea834b5ca6191b9d49bf4fcce68c3c0a6f
Billy Keyes <bluekeyes@gmail.com>
2017-07-04 13:15:29 -0700

    Add formatting function for messages

--- src/WebMidi/Event.elm
+++ src/WebMidi/Event.elm
@@ -1,11 +1,16 @@
 module WebMidi.Event exposing
     ( Event
     , Channel, Message(..)
+    , formatMessage
     )
 
 {-| Event type for the Web MIDI API.
 
 @docs Event, Channel, Message
+
+# Display Functions
+
+@docs formatMessage
 
 -}
 
@@ -49,3 +54,51 @@ type Message
     | StopSequence
     | ActiveSensing
     | Reset
+
+
+{-| Creates a string representation of a message. Useful for debugging or
+logging messages.
+-}
+formatMessage : Message -> String
+formatMessage m =
+    let
+        fmt name fields =
+            name ++ "[" ++ String.join ", " (List.map (\(k, v) -> k ++ ": " ++ toString v) fields) ++ "]"
+    in
+        case m of
+            NoteOff c note vel ->
+                fmt "NoteOff" [("chan", c), ("note", note), ("vel", vel)]
+            NoteOn c note vel ->
+                fmt "NoteOn" [("chan", c), ("note", note), ("vel", vel)]
+            KeyPressure c note val ->
+                fmt "KeyPressure" [("chan", c), ("note", note), ("val", val)]
+            ControlChange c ctrl val ->
+                fmt "ControlChange" [("chan", c), ("ctrl", ctrl), ("val", val)]
+            ProgramChange c prgm ->
+                fmt "ProgramChange" [("chan", c), ("prgm", prgm)]
+            ChannelPressure c val ->
+                fmt "ChannelPressure" [("chan", c), ("val", val)]
+            PitchBend c val ->
+                fmt "PitchBend" [("chan", c), ("val", val)]
+            SysEx data ->
+                fmt "SysEx" [("bytes", List.length data)]
+            TimeCodeQuarterFrame typ val ->
+                fmt "TimeCodeQuarterFrame" [("type", typ), ("val", val)]
+            SongPosition pos ->
+                fmt "SongPosition" [("pos", pos)]
+            SongSelect song ->
+                fmt "SongPosition" [("song", song)]
+            TuneRequest ->
+                "TuneRequest"
+            TimingClock ->
+                "TimingClock"
+            StartSequence ->
+                "StartSequence"
+            ContinueSequence ->
+                "ContinueSequence"
+            StopSequence ->
+                "StopSequence"
+            ActiveSensing ->
+                "ActiveSensing"
+            Reset ->
+                "Reset"
